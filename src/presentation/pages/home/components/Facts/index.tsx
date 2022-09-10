bd27856113fb46b91455f68e548d0b8469c83632
felcg <xfelipecg@gmail.com>
2022-09-10 13:34:08 -0300

    feat(presentation): create Facts component

---
+++ src/presentation/pages/home/components/Facts/index.tsx
@@ -0,0 +1,21 @@
+import { FactsContext } from "presentation/context";
+import { Props } from "presentation/protocols";
+import React, { useContext } from "react";
+import Spinner from "../Spinner";
+import "./facts-styles.scss";
+
+const Facts: React.FC<Props> = ({ children }) => {
+  const { loading } = useContext(FactsContext);
+
+  return (
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <section className="factsContainer">{children}</section>
+      )}
+    </>
+  );
+};
+
+export default Facts;
