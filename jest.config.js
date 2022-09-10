6d0156d4a562a9d51131a6009f1abec1c79b47f8
felcg <xfelipecg@gmail.com>
2022-09-10 19:13:12 -0300

    chore: update jest.config collectCoverageFrom

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   roots: ["<rootDir>/src", "<rootDir>/tests"],
   collectCoverageFrom: [
     "<rootDir>/src/**/*.{ts,tsx}",
+    "!<rootDir>/src/**/index.{ts,tsx}",
     "!<rootDir>/src/index.{ts,tsx}",
+    "!<rootDir>/src/infra/**/**/urls.ts",
+    "!<rootDir>/src/**/index.ts",
+    "!**/*.d.ts",
   ],
   coverageDirectory: "coverage",
   setupFilesAfterEnv: ["<rootDir>/src/main/config/setupTests.ts"],
