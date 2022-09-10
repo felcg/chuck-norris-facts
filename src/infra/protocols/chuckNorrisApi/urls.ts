ac7e3142d6bfb4fa2f43e8b6c095d1cf443839b2
felcg <xfelipecg@gmail.com>
2022-09-10 13:27:51 -0300

    feat(infra): add axiosHttpClient and chuckNorrisApi protocols to infra layer

---
+++ src/infra/protocols/chuckNorrisApi/urls.ts
@@ -0,0 +1,5 @@
+export enum ChuckNorrisApi {
+  searchUrl = "https://api.chucknorris.io/jokes/search",
+  random = "https://api.chucknorris.io/jokes/random",
+  categories = "https://api.chucknorris.io/jokes/categories"
+}
